3208acfbc13fbcf9380a6f7b344bc5b5cbc504c9
georgprok <georgprok@mail.ru>
2026-02-07 01:39:25 +0300

    add variables example

---
+++ src/Variables.java
@@ -0,0 +1,24 @@
+public class Variables {
+    public static void main(String[] args){
+        //Целые
+        int age = 25;
+        long population = 800000000000L;
+
+        // Дробные
+        float price = 99.99f;
+        double mark = 45.55;
+
+        // Строка
+        String name = "Ваня";
+
+        // Boolean
+        boolean isStudent = true;
+
+        // Вывод
+        System.out.println("Имя: " + name);
+        System.out.println("Возраст: " + age);
+        System.out.println("Цена: " + price);
+        System.out.println("Оценка: " + mark);
+        System.out.println("Студент: " + isStudent);
+    }
+}
